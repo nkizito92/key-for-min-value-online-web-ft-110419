27d380f2c2cf1dfa607e3b5e1a99d15131dc3ab0
Kizito Njoku <nkizito57@gmail.com>
2019-11-09 02:12:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   name_hash.each do |key, value |
     for i in name_hash[i] < name_hash.length
-      if name_hash[i] < minar[1]
+      if name_hash[i].to_i < minar[0]
         
   end 
 end
