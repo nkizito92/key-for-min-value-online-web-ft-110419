66594b8c95e9a6d043db12f1869fe317258b74d4
Kizito Njoku <nkizito57@gmail.com>
2019-11-09 02:15:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  minar = name_hash[0]
   name_hash.each do |key, value |
     key
-  end 
-   for i in name_hash[i] < name_hash.length
-      if name_hash[i].to_i < minar[0]
-        minar = name_hash[i]
-      end
-    end 
+  end
 end
