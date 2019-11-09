271ea6df98ab839e02eeafeeb2d4d42b0749d4eb
Kizito Njoku <nkizito57@gmail.com>
2019-11-09 02:21:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,7 @@ def key_for_min_value(name_hash)
   min = "chicken"
   key = nil
   name_hash.collect do |name, value |
-    if min == "chicken"
-      min = value
-      key = name
-    elsif min > value
+    if min == "chicken" || min > value
       min = value
       key = name
     end 
